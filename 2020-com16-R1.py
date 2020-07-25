fb56d6dd637dc3e320a3183dee358a736e8603ba
Felipe Morales <felipe.morales.querol@gmail.com>
2020-07-25 00:32:16 -0300

    Actualización de archivo

--- 2020-com16-R1.py
+++ 2020-com16-R1.py
@@ -1,18 +1,33 @@
 # ¡SE RECOMIENDA LEER EL EXAMEN COMPLETO Y LUEGO ELABORAR UNA ESTRATEGIA PARA SU RESOLUCIÓN!
 
 # 1) Programar en Python (20 puntos):
-# a) Pedir el ingreso de los 4 números de una tarjeta de débito y/o de crédito y imprimir dicha información con el siguiente formato: **** **** **** 9999 (9999 representa al último número ingresado). Por ejemplo si ingresa 1256 8956 7825 2589, deberá imprimir **** **** **** 2589. En caso que algunos de los números no tenga 4 dígitos, imprimir el cartel “Los números ingresados no se pueden procesar.”
+print("Ejercicio 1a")
+# a) Pedir el ingreso de los 4 números de una tarjeta de débito y/o de crédito y imprimir dicha información
+#    con el siguiente formato: **** **** **** 9999 (9999 representa al último número ingresado). 
+#    Por ejemplo si ingresa 1256 8956 7825 2589, deberá imprimir **** **** **** 2589. 
+#    En caso que algunos de los números no tenga 4 dígitos, imprimir el cartel “Los números ingresados no 
+#    se pueden procesar.”
 
-# b) Ingresar por teclado el alto, largo y ancho de una habitación, y calcular la superficie total para pintar sus paredes y techo. Además dicha habitación tiene ocupado 5m2 en aberturas, las cuales NO se deben pintar. Deberá controlar que la superficie sea mayor que 5m2 para poder pintar, si no imprimir “No se puede determinar la superficie a pintar”.
+
+# b) Ingresar por teclado el alto, largo y ancho de una habitación, y calcular la superficie total para 
+#    pintar sus paredes y techo. Además dicha habitación tiene ocupado 5m2 en aberturas, las cuales 
+#    NO se deben pintar. Deberá controlar que la superficie sea mayor que 5m2 para poder pintar, 
+#    si no imprimir “No se puede determinar la superficie a pintar”.
 
 # 2) Programar en Python las siguientes funciones con sus respectivas invocaciones (40 puntos):
-# Programe una función de nombre superficie_habitacion_a_pintar, que recibe por parámetros el alto, largo, ancho y superficie de aberturas -no pintar-. La función debe devolver los metros totales para pintar sus paredes y techo. En caso de que la superficie a pintar esté por debajo de la superficie a omitir, devolver -1. (30 puntos)
-# Ejemplo: superficie_habitacion_a_pintar(3, 10, 5, 200) deberá devolver -1.
+# a) Programe una función de nombre superficie_habitacion_a_pintar, que recibe por parámetros el alto, largo, 
+#    ancho y superficie de aberturas -no pintar-. La función debe devolver los metros totales para pintar 
+#    sus paredes y techo. En caso de que la superficie a pintar esté por debajo de la superficie a omitir, 
+#    devolver -1. (30 puntos)
+#    Ejemplo: superficie_habitacion_a_pintar(3, 10, 5, 200) deberá devolver -1.
 
-# Programe una función de nombre imprimir_mayores_que, que reciba una lista de números y un número tope, la cual debe imprimir por pantalla aquellos números de la lista mayores que el número tope ingresado (10 puntos).
-# Ejemplo: imprimir_mayores_que([2, 4, -67, 9], 0) deberá imprimir 2, 4 ,9.
+# b) Programe una función de nombre imprimir_mayores_que, que reciba una lista de números y un número tope,
+#    la cual debe imprimir por pantalla aquellos números de la lista mayores que el número tope ingresado 
+#    (10 puntos).
+#    Ejemplo: imprimir_mayores_que([2, 4, -67, 9], 0) deberá imprimir 2, 4 ,9.
 
-# 3) Realice un programa para una plataforma online a través del cual pueda ingresar el nombre de las personas, curso a realizar y su edad. La carga finaliza cuando se ingresa el nombre ‘ZZZ’. 
-# Luego, deberá imprimir las edades mayores a 18.
-# Por último calcular el promedio de edades de las personas que cursen “Machine Learning”. (40 puntos).
-# Nota: Se recomienda utilizar funciones!
+# 3) Realice un programa para una plataforma online a través del cual pueda ingresar el nombre de las 
+#    personas, curso a realizar y su edad. La carga finaliza cuando se ingresa el nombre ‘ZZZ’. 
+#    Luego, deberá imprimir las edades mayores a 18.
+#    Por último calcular el promedio de edades de las personas que cursen “Machine Learning”. (40 puntos).
+#    Nota: Se recomienda utilizar funciones!
